f2a3ff324ccc56f4a56807bbf64b3914e484fbc9
nicomelendez <nicolas.melendez@estudiantes.utec.edu.uy>
2023-07-07 19:20:18 -0300

    x

---
+++ src/data/book.js
@@ -0,0 +1,30 @@
+export const books = [
+    {
+        id: 'clean-code',
+        title: 'Clean Code',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+        opinion: "Un libro imprescindible para cualquier desarrollador. 'Clean Code' ofrece pautas claras y prácticas para escribir un código limpio y mantenible. Con ejemplos reales y principios sólidos, este libro mejorará tu habilidad para construir software de calidad.",
+        author: "Robert C. Martin"
+    },
+    {
+        id: 'the-clean-coder',
+        title: 'The Pragmatic Programmer: From Journeyman to Master',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+        opinion: "Un libro inspirador que enfatiza la importancia de la ética y la profesionalidad en la programación. 'The Pragmatic Programmer' está lleno de consejos prácticos y técnicas para mejorar tus habilidades de codificación y convertirte en un maestro en tu oficio. Es una lectura obligatoria para todo desarrollador comprometido en crecer profesionalmente.",
+        author: "Robert C. Martin"
+    },
+    {
+        id: 'design-patterns',
+        title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348027904i/85009.jpg',
+        opinion: "Una lectura esencial para comprender los patrones de diseño y cómo aplicarlos en el desarrollo de software. 'Design Patterns' presenta soluciones probadas a problemas comunes en el diseño de sistemas orientados a objetos. Escrito por cuatro expertos en el campo, este libro te ayudará a mejorar tus habilidades de diseño y arquitectura de software.",
+        author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides"
+    },
+    {
+        id: 'domain-driven-design',
+        title: 'Domain-Driven Design: Tackling Complexity in the Heart of Software',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1287493789i/179133.jpg',
+        opinion: "Un enfoque revolucionario para abordar la complejidad en el desarrollo de software. 'Domain-Driven Design' proporciona herramientas y técnicas para diseñar sistemas basados en el dominio, lo que te permitirá crear software que resuelva problemas complejos de manera efectiva. Escrito con claridad y experiencia, este libro es esencial para arquitectos y desarrolladores de software comprometidos con la calidad y el valor del negocio.",
+        author: "Eric Evans"
+    }
+];
